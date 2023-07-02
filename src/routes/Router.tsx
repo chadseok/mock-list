e960fe9337e5b63888326660f16f8c9246fae33b
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-02 18:09:36 +0900

    리스트 페이지에 loader연결
    리스트 페이지에 필요한 PostList에 관한 서버데이터 호출 연결

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,9 +2,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import List from "../pages/List";
 import Post from "../pages/Post";
 import About from "../pages/About";
+import { getPostList } from "./loader";
 
 const router = createBrowserRouter([
-  { path: "/list", element: <List /> },
+  { path: "/list", element: <List />, loader: getPostList },
   { path: "/post/:postId", element: <Post /> },
   { path: "/about", element: <About /> },
 ]);
