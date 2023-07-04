df2509b475ce9c941e6d8f671fb1a5b759a2ce56
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-04 21:50:56 +0900

    라우터에 loader연결
    새로 개편한 loader를 라우터와 연결하였습니다
    Post페이지와 About페이지는 params를 활용하여 loader를 작동하도록
    설계하였습니다.
    About페이지의 라우팅의 경우 userId params가 없어 추가하였습니다

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,12 +2,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import List from "../pages/List";
 import Post from "../pages/Post";
 import About from "../pages/About";
-import { getPostList } from "./loader";
+import { postLoader, listLoader, aboutLoader } from "./loader";
 
 const router = createBrowserRouter([
-  { path: "/list", element: <List />, loader: getPostList },
-  { path: "/post/:postId", element: <Post /> },
-  { path: "/about", element: <About /> },
+  { path: "/list", element: <List />, loader: listLoader },
+  {
+    path: "/post/:postId",
+    element: <Post />,
+    loader: ({ params }) => postLoader(params.postId!),
+  },
+  {
+    path: "/about/:userId",
+    element: <About />,
+    loader: ({ params }) => aboutLoader(params.userId!),
+  },
 ]);
 
 function RouterComponent() {
