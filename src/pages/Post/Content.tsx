a7672ca170ce1d299b6c9701b8f409cbb389da87
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-05 19:24:11 +0900

    Content컴포넌트 생성
    Post페이지의 섹션인 post글 내용에 해당하는 컴포넌트를 생성하였습니다
    Post페이지로 부터 props로 데이터를 전달받아 화면을 구성합니다

---
+++ src/pages/Post/Content.tsx
@@ -0,0 +1,12 @@
+import type { PostData } from "../../lib/types/main";
+
+const Content = ({ contentData }: { contentData: PostData }) => {
+  return (
+    <div>
+      <h2>{contentData.title}</h2>
+      <p>{contentData.body}</p>
+    </div>
+  );
+};
+
+export default Content;
