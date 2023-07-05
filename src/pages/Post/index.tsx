fa8e9e192bd7519b71ed099f3d64d983eb0b6991
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-05 19:29:04 +0900

    Content컴포넌트와 Comment컴포넌트를 Post페이지컴포넌트와 연결
    useLoaderData로 Content와 Comment에 해당하는 데이터를 받아 각각의 컴포넌트에
    props로 전달하는 형태로 설계하였습니다

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,5 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import Content from "./Content";
+import Comments from "./Comments";
+import type { CommentData, PostData } from "../../lib/types/main";
+
 const Post = () => {
-  return <div>Post</div>;
+  const postData = useLoaderData() as {
+    content: PostData;
+    comments: CommentData[];
+  };
+
+  return (
+    <div>
+      <Content contentData={postData.content} />
+      <Comments commentsData={postData.comments} />
+    </div>
+  );
 };
 
 export default Post;
