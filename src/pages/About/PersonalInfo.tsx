ea435a1a0ce1a887ee6c45ff2addfb70e8239c0a
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-08 18:13:13 +0900

    About페이지의 유저 개인 정보 섹션 컴포넌트 생성
    About페이지에 들어갈 유저 개인정보에 관한 섹션을
    테이블 형식으로 구성하였습니다

---
+++ src/pages/About/PersonalInfo.tsx
@@ -0,0 +1,28 @@
+import { PersonalData } from "../../lib/types/main";
+
+const PersonalInfo = ({ personalInfo }: { personalInfo: PersonalData }) => {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>name</td>
+          <td>{personalInfo?.name}</td>
+          <td>phone</td>
+          <td>{personalInfo.phone}</td>
+        </tr>
+        <tr>
+          <td>company</td>
+          <td>{personalInfo.company?.name}</td>
+          <td>email</td>
+          <td>{personalInfo.email}</td>
+        </tr>
+        <tr>
+          <td>website</td>
+          <td>{personalInfo.website}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default PersonalInfo;
