47119cc92cede2f2a0976fc99870db1cc8e0a512
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-11 18:51:14 +0900

    PostPreview컴포넌트 css 추가

--- src/pages/List/PostPreview.tsx
+++ src/pages/List/PostPreview.tsx
@@ -3,10 +3,10 @@ import type { PostData } from "lib/types/main";
 
 const PostPreview = ({ postData }: { postData: PostData }) => {
   return (
-    <div>
+    <div className="w-[40rem] px-4 py-6 border-b border-solid border-slate-100">
       <Link to={`/post/${postData.id}`}>
-        <h3>{postData.title}</h3>
-        <p>{postData.body}</p>
+        <h3 className="text-xl mb-2">{postData.title}</h3>
+        <p className="text-slate-500 text-sm">{postData.body}</p>
       </Link>
     </div>
   );
