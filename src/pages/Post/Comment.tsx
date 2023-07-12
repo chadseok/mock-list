416bbf5d78bb634f8c37df104afd47a78ed63260
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-12 18:39:10 +0900

    Comment컴포넌트 css 적용

--- src/pages/Post/Comment.tsx
+++ src/pages/Post/Comment.tsx
@@ -2,10 +2,10 @@ import type { CommentData } from "lib/types/main";
 
 const Comment = ({ commentData }: { commentData: CommentData }) => {
   return (
-    <div>
-      <div>
-        <span>{commentData.name}</span>
-        <span>{commentData.email}</span>
+    <div className="flex flex-col gap-2 mb-5 border border-solid border-slate-300 rounded-md p-4">
+      <div className="flex flex-col gap-1">
+        <span className="text-sm">{commentData.name}</span>
+        <span className="text-xs text-slate-500">{commentData.email}</span>
       </div>
       <div>{commentData.body}</div>
     </div>
