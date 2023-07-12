e7b1e482bbd223bee3b01b2e12d7b17d6c6a0c34
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-12 18:38:54 +0900

    Comments컴포넌트 css 적용

--- src/pages/Post/Comments.tsx
+++ src/pages/Post/Comments.tsx
@@ -1,10 +1,12 @@
+import { FaRegComment } from "react-icons/fa";
 import Comment from "./Comment";
 import type { CommentData } from "lib/types/main";
 
 const Comments = ({ commentsData }: { commentsData: CommentData[] }) => {
   return (
     <div>
-      <div>
+      <div className="flex gap-2 text-xl my-5 items-center">
+        <FaRegComment />
         <span>{commentsData.length}</span>
       </div>
       <div>
