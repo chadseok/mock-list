2f1b28b92eed41b47066dac041beb1c4ca8dd159
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-12 18:35:13 +0900

    Content컴포넌트 css적용

--- src/pages/Post/Content.tsx
+++ src/pages/Post/Content.tsx
@@ -3,8 +3,8 @@ import type { PostData } from "lib/types/main";
 const Content = ({ contentData }: { contentData: PostData }) => {
   return (
     <div>
-      <h2>{contentData.title}</h2>
-      <p>{contentData.body}</p>
+      <h2 className="text-4xl my-5">{contentData.title}</h2>
+      <p className="text-lg my-5">{contentData.body}</p>
     </div>
   );
 };
