6ce7d3fbe7243d3187f4c547b4f82e8fe9a904f1
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-18 22:54:10 +0900

    Pagination컴포넌트 기능 구현

--- src/pages/List/Pagination.tsx
+++ src/pages/List/Pagination.tsx
@@ -1,5 +1,37 @@
-const Pagination = () => {
-  return <div>Pagination</div>;
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { PAGE_DIVIDER, TOTAL_PAGE } from "lib/constant/main";
+
+type Props = {
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+};
+
+const Pagination = ({ page, setPage }: Props) => {
+  const pageBundle = Array(PAGE_DIVIDER)
+    .fill(0)
+    .map(
+      (_, i) => Math.floor((page - 1) / PAGE_DIVIDER) * PAGE_DIVIDER + i + 1
+    );
+
+  const moveBackward = () => {
+    if (page === 1) return;
+    setPage(page - 1);
+  };
+
+  const moveForward = () => {
+    if (page === TOTAL_PAGE) return;
+    setPage(page + 1);
+  };
+
+  return (
+    <div>
+      <FaAngleLeft onClick={moveBackward} />
+      {pageBundle.map((pageNum) => (
+        <button onClick={() => setPage(pageNum)}>{pageNum}</button>
+      ))}
+      <FaAngleRight onClick={moveForward} />
+    </div>
+  );
 };
 
 export default Pagination;
