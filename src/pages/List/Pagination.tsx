626fcb347454158962b8c98b929b95d64148c104
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-21 17:32:13 +0900

    pagination 양옆 이동 아이콘 버튼 역할 변경
    기존의 양옆 이동 아이콘 버튼의 역할은 다음페이지로 가거나 이전페이지로
    가도록 하는 역할이었습니다. 하지만 다음페이지로 가거나 이전페이지로 가는
    것은 특정 버튼을 눌러서 갈 수 있는 것이기 때문에 이 아이콘의 역할을
    다음 페이지 묶음의 페이지로 갈 수 있도록 하는 것으로 바꾸었습니다

--- src/pages/List/Pagination.tsx
+++ src/pages/List/Pagination.tsx
@@ -7,27 +7,31 @@ type Props = {
 };
 
 const Pagination = ({ page, setPage }: Props) => {
+  const pageBundleNum = Math.floor((page - 1) / PAGE_DIVIDER);
+  const LAST_PAGE_BUNDLE = Math.floor((TOTAL_PAGE - 1) / PAGE_DIVIDER);
   const pageBundle = Array(PAGE_DIVIDER)
     .fill(0)
-    .map(
-      (_, i) => Math.floor((page - 1) / PAGE_DIVIDER) * PAGE_DIVIDER + i + 1
-    );
+    .map((_, i) => pageBundleNum * PAGE_DIVIDER + 1 + i);
 
   const moveBackward = () => {
-    if (page === 1) return;
-    setPage(page - 1);
+    if (pageBundleNum === 0) return;
+    setPage((pageBundleNum - 1) * PAGE_DIVIDER + 1);
   };
 
   const moveForward = () => {
-    if (page === TOTAL_PAGE) return;
-    setPage(page + 1);
+    if (pageBundleNum === LAST_PAGE_BUNDLE) return;
+    setPage((pageBundleNum + 1) * PAGE_DIVIDER + 1);
   };
 
   return (
     <div className="flex gap-8 justify-center items-center mt-8">
-      <button onClick={moveBackward}>
-        <FaAngleLeft size={24} className="hover:scale-150 duration-300" />
-      </button>
+      {pageBundleNum === 0 ? (
+        <button className="w-8 h-8"></button>
+      ) : (
+        <button onClick={moveBackward}>
+          <FaAngleLeft size={24} className="hover:scale-150 duration-300" />
+        </button>
+      )}
       {pageBundle.map((pageNum) => (
         <button
           className={`w-8 h-8 rounded-md ${
@@ -38,9 +42,13 @@ const Pagination = ({ page, setPage }: Props) => {
           {pageNum}
         </button>
       ))}
-      <button onClick={moveForward}>
-        <FaAngleRight size={24} className="hover:scale-150 duration-300" />
-      </button>
+      {pageBundleNum === LAST_PAGE_BUNDLE ? (
+        <button className="w-8 h-8"></button>
+      ) : (
+        <button onClick={moveForward}>
+          <FaAngleRight size={24} className="hover:scale-150 duration-300" />
+        </button>
+      )}
     </div>
   );
 };
