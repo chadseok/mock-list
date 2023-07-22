eaba11f9f5ed56777742909fb9cd27c82b2502f7
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-22 17:48:49 +0900

    PersonalInfo css 추가

--- src/pages/About/PersonalInfo.tsx
+++ src/pages/About/PersonalInfo.tsx
@@ -2,22 +2,22 @@ import type { PersonalData } from "lib/types/main";
 
 const PersonalInfo = ({ personalInfo }: { personalInfo: PersonalData }) => {
   return (
-    <table>
+    <table className="w-full my-8">
       <tbody>
         <tr>
-          <td>name</td>
+          <td className="font-bold text-slate-500">name</td>
           <td>{personalInfo?.name}</td>
-          <td>phone</td>
+          <td className="font-bold text-slate-500">phone</td>
           <td>{personalInfo.phone}</td>
         </tr>
         <tr>
-          <td>company</td>
+          <td className="font-bold text-slate-500">company</td>
           <td>{personalInfo.company?.name}</td>
-          <td>email</td>
+          <td className="font-bold text-slate-500">email</td>
           <td>{personalInfo.email}</td>
         </tr>
         <tr>
-          <td>website</td>
+          <td className="font-bold text-slate-500">website</td>
           <td>{personalInfo.website}</td>
         </tr>
       </tbody>
