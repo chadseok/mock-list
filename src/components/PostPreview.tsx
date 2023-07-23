23e7c282d2a31175c55f0933f1247a1ada20f0f1
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-07-23 15:26:00 +0900

    PostPreview css변경
    PostPreview컴포넌트가 여러 페이지에서 쓰이기 때문에
    특정 길이로 고정시키지 않도록 길이를 부모 요소를 따라가도록
    변경하였습니다. 또한 PostPreview사이의 간격을 좀 더 늘려
    시야에 많은 PostPreview가 들어오지 않도록 하였습니다

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -3,7 +3,7 @@ import type { PostData } from "lib/types/main";
 
 const PostPreview = ({ postData }: { postData: PostData }) => {
   return (
-    <div className="w-[40rem] px-4 py-6 border-b border-solid border-slate-100">
+    <div className="w-full py-8 border-b border-solid border-slate-100">
       <Link to={`/post/${postData.id}`}>
         <h3 className="text-xl mb-2">{postData.title}</h3>
         <p className="text-slate-500 text-sm">{postData.body}</p>
