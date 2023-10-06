fef5d24589b4651ef3e2d16ce175b9bcd276b4ba
changhwan seok <83488329+chadseok@users.noreply.github.com>
2023-10-06 22:27:01 +0900

    timeout 시간이 너무 짧다고 판단해 5000으로 수정
    timeout으로 판단하는  시간이 1초면 너무 짧다고 생각해
    5초로 변경

--- src/routes/loader.ts
+++ src/routes/loader.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
-  timeout: 1000,
+  timeout: 5000,
 });
 
 export const listLoader = async () => {
